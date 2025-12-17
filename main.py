c52973829351ff5a78fcf37c3d5068cd2130408e
AndrZva <andrey.zverev21@gmail.com>
2025-12-17 22:34:06 +0800

    My portfolio
    yes, my portfolio

--- main.py
+++ main.py
@@ -1,53 +1,26 @@
 #Импорт
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template,request, redirect
+
 
 
 app = Flask(__name__)
 
-#Результаты формы
-@app.route('/', methods=['GET','POST'])
+#Запуск страницы с контентом
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        # получаем выбранное изображение
-        selected_image = request.form.get('image-selector')
-
-        # Задание №2.Получаем текст
-        texttop = request.form.get('textTop')
-        textbottom = request.form.get('textBottom')
+    return render_template('index.html')
 
 
-        # Задание №3. Получаем расположение текста
-        text_top_y = request.form.get('textTop_y')
-        text_bottom_y = request.form.get('textBottom_y')
-       
-
-        # Задание №3. Получаем цвет текста
-        selected_color = request.form.get('color-selector')
-
-        return render_template('index.html', 
-                               # отображаем выбранное изображение
-                               selected_image=selected_image, 
-
-                               # Задание №2. Отображаем текст
-                               texttop=texttop,
-                               textbottom=textbottom,
-
-                               # Задание №3. Отображаем цвет 
-                               selected_color =selected_color,
-                               
-                               #Задание №3. Отоброжаем расположение текста
-                               text_top_y = text_top_y,
-                               text_bottom_y = text_bottom_y
-                               
-
-                               )
-    else:
-        # отображаем первое изображение по умолчанию
-        return render_template('index.html', selected_image='logo.svg')
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_telegram = request.form.get('button_telegram')
+    button_unity = request.form.get('button_unity')
+    button_web = request.form.get('button_web')
+    return render_template('index.html', button_python=button_python, button_telegram=button_telegram,
+                           button_unity=button_unity, button_web=button_web)
 
 
-@app.route('/static/img/<path:path>')
-def serve_images(path):
-    return send_from_directory('static/img', path)
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
